1dc5af11824bdcd0938f916e1a048004aee8a5d8
Miguel Mateo <miguelmateot94@gmail.com>
2026-04-20 15:25:15 -0400

    feat(style): improve the style of the page

--- src/app/modules/issues/pages/issues-list-page.ts
+++ src/app/modules/issues/pages/issues-list-page.ts
@@ -6,14 +6,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   template: `
     <section class="grid grid-cols-1 sm:grid-cols-3 gap-4">
       <header class="col-span-3">
-        <h1>Issues List Page</h1>
+        <h1 class="text-3xl font-bold">Github Issues</h1>
         <div class="divider"></div>
       </header>
       <article class="col-span-3 flex flex-col-3">
         <div class="flex gap-2">
           <button class="btn btn-primary">All</button>
-          <button class="btn btn-ghost">Open</button>
-          <button class="btn btn-ghost">Closed</button>
+          <button class="btn btn-outline">Open</button>
+          <button class="btn btn-outline">Closed</button>
         </div>
       </article>
       <article class="mt-4 flex flex-col col-span-2">
